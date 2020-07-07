57e058209eda5078f15d47fcd57bb10d373c88b7
mlee03 <Mikyung.Lee@noaa.gov>
2020-07-07 11:10:37 -0400

    adds workflow.sh to prevent concurrent runs

---
+++ workflow.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+wfile=workflow_status/workflow_status
+
+
+whereami=$1
+work_id=$2
+
+
+if [ $whereami == 'start' ] ; then
+    
+    if [ -f $wfile ] ; then
+        job_status=$( awk '{print $2}' $wfile )
+        if [ $job_status == 'RUNNING' ] ; then
+            cat $wfile
+            exit 1
+        else
+            rm -rf $wfile
+            echo " $work_id RUNNING `date` " > $wfile            
+        fi
+    else
+        mkdir -p workflow_status
+        echo " $work_id RUNNING `date` " > $wfile
+    fi
+
+elif [ $whereami == 'failed' ] ; then
+
+    rm -rf $wfile
+    echo " $work_id DONE `date` " > $wfile
+
+
+elif [ $whereami == 'end' ] ; then
+
+    rm -rf $wfile
+    echo " $work_id DONE `date` " > $wfile
+
+fi
+            
+    
