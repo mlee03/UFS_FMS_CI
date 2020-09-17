d4212e87608c047147151265eb63585aaa4d3eb9
mlee03 <Mikyung.Lee@noaa.gov>
2020-09-17 13:11:15 -0400

    updates to a cleaner workflow

--- check.sh
+++ check.sh
@@ -1,44 +1,32 @@
 #!/bin/bash
 
-#set -x
-
 #: relevant files 
-ncep_hashfile=$PWD/"hashfiles/hashfile_ncep"
-ufs_hashfile=$PWD/"hashfiles/hashfile_ufs"
-fms_hashfile=$PWD/"hashfiles/hashfile_fms"
+ufs_hashfile=$PWD/"hashfiles/hashfile_ufs"  #: on Github cache
+fms_hashfile=$PWD/"hashfiles/hashfile_fms"  #: on Github cache
+nceplib_hashfile=$PWD/"LIB_HASHFILE"        #: in branch build_libs
 logfile=$PWD/LOG
 
 
-#: ncep external library version
-external_v='ufs-v1.0.0'
-
-
 #: repositories
-ncep_upstream='https://github.com/NOAA-EMC/NCEPLIBS.git'
 ufs_upstream='https://github.com/ufs-community/ufs-weather-model.git'
 fms_upstream='https://github.com/NOAA-GFDL/FMS.git'
 
 
 #: branches
-ncep_branch="release/public-v1"  ; ncep_hashtag="refs/heads/$ncep_branch"
-ufs_branch="develop"             ; ufs_hashtag="refs/heads/$ufs_branch"
-fms_branch="master"              ; fms_hashtag='HEAD'
+ufs_branch="develop"  ; ufs_hashtag="refs/heads/$ufs_branch"
+fms_branch="master"   ; fms_hashtag='HEAD'
 
 
-#: default build to be true
-ncep_build='true'
-ufs_build='true'
-fms_build='true'
+#: nceplibs hash 
+curr_ncep=$( grep -m 1 "NCEPLIBS" $nceplib_hashfile | awk '{print $3}' )
 
 
-#: force build
-ncep_override='false'
-ufs_override='false'
-fms_override='false'
+#: default build to be true  ;  force build set to false 
+ufs_build='true'  ;  ufs_override='false'
+fms_build='true'  ;  fms_override='false'
 
 
 #: restart hashfiles
-ncep_cleanhashfile='false'
 ufs_cleanhashfile='false'
 fms_cleanhashfile='false'
 
@@ -47,12 +35,11 @@ fms_cleanhashfile='false'
 fms_nhash=$3
 
 
-#: make hashfiles if they don't exist
-[ $ncep_cleanhashfile == 'true' ] && rm $ncep_hashfile
+#: remove existing hashfiles if cleanhasfile set to true
 [ $ufs_cleanhashfile == 'true' ]  && rm $ufs_hashfile
 [ $fms_cleanhashfile == 'true' ]  && rm $fms_hashfile
 
-[ ! -f $ncep_hashfile ] && touch -a $ncep_hashfile
+#: make hashfiles if they don't exist
 [ ! -f $ufs_hashfile ]  && touch -a $ufs_hashfile
 [ ! -f $fms_hashfile ]  && touch -a $fms_hashfile
 
@@ -67,34 +54,22 @@ ncep_dockerpulled='false'
 
 
 #: get curr hashes
-curr_ncep=$( git ls-remote $ncep_upstream | grep "$ncep_hashtag" | awk '{print $1}' )
 curr_ufs=$( git ls-remote  $ufs_upstream  | grep "$ufs_hashtag"  | awk '{print $1}' )
 curr_fms=$( git ls-remote  $fms_upstream  | grep "$fms_hashtag"  | awk '{print $1}' )
 
 
 #: echo to log
 echo `date`                            > $logfile
-echo "NCEP REPOSITORY $ncep_upstream" >> $logfile
 echo "UFS  REPOSITORY $ufs_upstream"  >> $logfile
 echo "FMS  REPOSITORY $fms_upstream"  >> $logfile
 echo ''                               >> $logfile
-echo "NCEP BRANCH $ncep_branch"       >> $logfile
 echo "UFS  BRANCH $ufs_branch"        >> $logfile
 echo "FMS  BRANCH $fms_branch"        >> $logfile
 echo ''                               >> $logfile
-echo "NCEP NEW HASH $curr_ncep"       >> $logfile
 echo "UFS  NEW HASH $curr_ufs"        >> $logfile
 echo "FMS  NEW HASH $curr_fms"        >> $logfile
 echo ''                               >> $logfile
 
-
-#: build ncep if image doesn't exist
-if [ $( grep -c "PASSED $curr_ncep" $ncep_hashfile ) -ne 0 ] ; then
-    ncep_build='false'
-    echo "NCEP $curr_ncep passed already" 
-    echo "NCEP $curr_ncep passed already" >> $logfile
-    
-fi
 
 #: build ufs if log cannot be found
 if [ $( grep -c "PASSED $curr_ufs" $ufs_hashfile ) -ne 0 ] ; then
@@ -112,56 +87,9 @@ fi
 
 
 #: override
-[ $ncep_override == 'true' ] && ncep_build='true'
 [ $ufs_override == 'true' ]  && ufs_build='true'
 [ $fms_override == 'true' ]  && fms_build='true'
 
-
-#: build if set to true
-if [ $ncep_build == 'true' ] ; then
-
-    echo "**************************"
-    echo "BUILDING NCEPLIBS:$curr_ncep DOCKER IMAGE"
-    echo "**************************"
-    
-    docker pull mklee03/nceplibs:$curr_ncep
-    if [ $? -eq 0 ] ; then        
-        echo "NCEP $curr_ncep image exists"
-        echo "NCEP $curr_ncep image exists" >> $logfile
-        #: check record is updated
-        echo -e "PASSED $curr_ncep `date`\n $(cat $ncep_hashfile)" > $ncep_hashfile
-        ncep_dockerpulled='true'
-    else
-        git clone --recursive --branch $ncep_branch $ncep_upstream
-        cd NCEPLIBS && git checkout $curr_ncep
-        docker pull mklee03/external_libs:$external_v
-
-        sed -i "s/CHANGEME/$external_v/" ../NCEP_Dockerfile
-        docker build -f ../NCEP_Dockerfile -t nceplibs:$curr_ncep  .
-        
-        if [ $? -ne 0 ] ; then
-            echo -e "FAILED $curr_ncep `date`\n $(cat $ncep_hashfile)" > $ncep_hashfile
-            echo "***********************************"
-            echo "NCEP FAILED NCEP FAILED NCEP FAILED"
-            echo "***********************************"
-            cat $logfile
-            echo "(FAILED) COMMIT MESSAGE" 
-            git show -s $curr_ncep
-            exit $?
-        else
-            echo -e "PASSED $curr_ncep `date`\n $(cat $ncep_hashfile)" > $ncep_hashfile
-            echo "(PASSED) NCEP COMMIT MESSAGE" >> $logfile
-            git show -s $curr_ncep         >> $logfile
-            echo "***********************" >> $logfile
-        fi
-        
-        echo "$docker_password" | docker login -u $docker_username --password-stdin
-        docker tag nceplibs:$curr_ncep mklee03/nceplibs:$curr_ncep
-        docker push mklee03/nceplibs:$curr_ncep
-        cd ../
-        ufs_build=true        
-    fi
-fi    
     
 #: compile UFS if UFS has been updated
 if [ $ufs_build == 'true' ] ; then
@@ -267,14 +195,11 @@ cd ..
 
 
 echo -e "**********************\n\n" >> $logfile
-echo "NCEP HASHFILE" >> $logfile
-cat $ncep_hashfile >> $logfile
-
-echo -e "**********************\n\n" >> $logfile
 echo "UFS HASHFILE" >> $logfile
 cat $ufs_hashfile >> $logfile
 
 echo -e "**********************\n\n" >> $logfile
 echo "FMS HASHFILE" >> $logfile
 cat $fms_hashfile >> $logfile
+
 
--- reset_workflowfile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-wfile=workflow_status/workflow_status
-rm -rf $wfile
-
-mkdir -p workflow_status
-echo "FILE MANUALLY UPDATED `date`" > $wfile
-
-
