87bd9c9c791abb5b6780705ec84ea2343bf1f02f
Mikyung Lee <mlee@Orion-login-1.HPC.MsState.Edu>
2021-01-05 14:58:43 -0600

    update scripts

--- README.md
+++
@@ -1,8 +0,0 @@
-# UFS_FMS_CI
-
-To run the UFS weather model on Orion
-1.  git clone --branch orion_runscript https://github.com/mlee03/UFS_FMS_CI.git run_ufs
-2.  cd run_ufs
-3.  ./run_weather_model  
-4.  ./run_nccmp
-
---
+++ check_results.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+TESTDIR=CHANGEME_WORKDIR
+BASELINE=CHANGEME_BASELINE
+RESULTS=RESULTS
+
+#: check for nccmp
+if [ ! $(command -v nccmp) ] ; then
+    echo "NCCMP not found"
+    exit
+fi
+
+mkdir -p $RESULTS && cd $RESULTS
+
+
+#: log
+LOG=$PWD/RESULTS
+touch -a $LOG  && echo `date` >> $LOG
+echo "BASELINE: $BASELINE"    >> $LOG
+echo "TESTDIR:  $TESTDIR"     >> $LOG
+
+
+#: get fail_test in each directory
+echo -e "\n************************" >> $LOG
+echo "BASELINE fail_test" >> $LOG
+[ -f $BASELINE/fail_test ] && cat $BASELINE/fail_test >> $LOG 
+echo "TESTDIR fail_test" >> $LOG
+[ -f $TESTDIR/fail_test ] &&  cat $TESTDIR/fail_test >> $LOG
+echo -e "************************\n" >> $LOG
+
+
+BASELINE=$BASELINE/log_orion.intel
+TESTDIR=$TESTDIR/log_orion.intel
+
+
+#: compare results
+echo "NUMBER OF REGRESSION TESTS IN BASELINE" $(ls $BASELINE/rt*.log | grep -c ".log") >> $LOG
+echo "NUMBER OF REGRESSION TESTS IN TESTDIR " $(ls $TESTDIR/rt*.log  | grep -c ".log") >> $LOG
+
+
+for rtfile in $BASELINE/rt*.log ; do
+
+    rtfile2=${rtfile#$BASELINE"/rt_0"[0-9][0-9]"_"}
+    basedir2=$( grep "working dir" $BASELINE/rt*$rtfile2 | awk '{print $4}' )
+    testdir2=$( grep "working dir" $TESTDIR/rt*$rtfile2  | awk '{print $4}' )
+
+    output=NCCMP_$rtfile2 ; touch -a $output
+    for base_nc in $basedir2/*.nc ; do
+	ncfile=${base_nc#$basedir2"/"}
+	echo "***********************************************" >> $output
+	echo $ncfile >> $output
+	( nccmp -f -c 1 -d -m $basedir2/$ncfile $testdir2/$ncfile ) >> $output 2>&1
+    done
+
+    sed -i "s:$basedir2:BASELINE:g" $output
+    sed -i "s:$testdir2:TEST:g" $output
+    
+done    
+    
+
+
+
+
